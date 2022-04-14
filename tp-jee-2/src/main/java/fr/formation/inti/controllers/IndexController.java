3d8237df44a09370825940f42fda18bc4fe8dfe8
nico9 <nicolas.thlt@gmail.com>
2022-04-14 09:44:42 +0200

    edit Post index : for test

--- tp-jee-2/src/main/java/fr/formation/inti/controllers/IndexController.java
+++ tp-jee-2/src/main/java/fr/formation/inti/controllers/IndexController.java
@@ -35,7 +35,7 @@ public class IndexController extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		
 		doGet(request, response);
 	}
 
